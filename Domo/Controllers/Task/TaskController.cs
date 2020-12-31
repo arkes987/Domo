a4a0cacfb6f6d24de2a0137867e679f252f4a3c2
Arkadiusz Kina <arkadiusz.kina@pentacomp.pl>
2020-12-31 13:13:21 +0100

    Added sample controller

---
+++ Domo/Controllers/Task/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Domo.Controllers.Task
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly ILogger<TaskController> _logger;
+
+        public TaskController(ILogger<TaskController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
+
+
